bcfd5b8fe4970815ce6f5651204172be541827da
sc <sau.mia206@icloud.com>
2025-07-24 02:08:29 -0700

    Force rebuild and re-sync to S3

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
             }}>
             <h1 style={{ fontSize: '2.5rem', fontWeight: 'normal',marginBottom: '0.3rem'}}>Hello, World! My name is</h1>
             <h1 style={{ fontSize: '4rem', fontWeight: 'normal', margin: '0'}}>Saumya Chourasia</h1>
-            <h1 style={{ fontSize: '4rem', fontWeight: 'normal', margin: '0'}}>AUTO DEPLOY WORKS!!!</h1>
+            <h1 style={{ fontSize: '4rem', fontWeight: 'normal', margin: '0'}}>AUTO DEPLOY WORKS!!</h1>
 
 
             <a href="contact" className="home-contact-button"> Contact Me</a>
